feb29c9cfcc8af4b1978af404fb9bfc4aebeb538
Artem Vorotnikov <artem@vorotnikov.me>
2020-12-17 19:00:34 +0300

    EthApi: more calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(clippy::large_enum_variant)]
+
+use ethereum::{Block, Header, Transaction};
 use primitive_types::H256;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeSet;
@@ -8,7 +11,20 @@ pub struct ForkData {
     pub forks: BTreeSet<u64>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BlockBody {
+    pub transactions: Vec<Transaction>,
+    pub ommers: Vec<Header>,
+}
+
 #[tarpc::service]
 pub trait EthApi {
     async fn forks() -> Result<ForkData, String>;
+
+    async fn best_block() -> Result<(H256, u64), String>;
+    async fn canonical_hash(number: u64) -> Result<Option<H256>, String>;
+
+    async fn block(hash: H256) -> Result<Option<Block>, String>;
+    async fn header(hash: H256) -> Result<Option<Header>, String>;
+    async fn body(hash: H256) -> Result<Option<Block>, String>;
 }
