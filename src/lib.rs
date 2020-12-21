2415a26ac40a108c134118f6c1d651f84e2d3b43
Artem Vorotnikov <artem@vorotnikov.me>
2020-12-21 07:52:42 +0300

    Remove EthApi::block

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::large_enum_variant)]
 
-use ethereum::{Block, Header, Transaction};
+use ethereum::{Header, Transaction};
 use primitive_types::H256;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeSet;
@@ -24,7 +24,6 @@ pub trait EthApi {
     async fn best_block() -> Result<(H256, u64), String>;
     async fn canonical_hash(number: u64) -> Result<Option<H256>, String>;
 
-    async fn block(hash: H256) -> Result<Option<Block>, String>;
     async fn header(hash: H256) -> Result<Option<Header>, String>;
     async fn body(hash: H256) -> Result<Option<BlockBody>, String>;
 }
